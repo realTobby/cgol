e7d20418d65959089903dd2329fd5d8cd9463a93
xSillusx <tkattanek00@gmail.com>
2018-12-04 09:35:19 +0100

    Update

--- cgol.js
+++ cgol.js
@@ -10,11 +10,11 @@ function make2DArray(cold, rows) {
 let grid;
 let cols;
 let rows;
-let resolution = 40;
+let resolution = 10;
 function setup(){
-	createCanvas(400,400);
+	createCanvas(600,600);
 	cols = width / resolution;
-	
+
 	rows = height / resolution;
 	grid = make2DArray(cols,rows);
 	for(let i = 0; i < cols; i++) {
@@ -24,8 +24,24 @@ function setup(){
 	}
 }
 
+function countN(grid, x, y) {
+	let sum = 0;
+	  for (let i = -1; i < 2; i++) {
+	    for (let j = -1; j < 2; j++) {
+	      let col = (x + i + cols) % cols;
+	      let row = (y + j + rows) % rows;
+	      sum += grid[col][row];
+	    }
+	  }
+	  sum -= grid[x][y];
+	  return sum;
+	}
+
 function draw() {
 	background(0);
+
+	let next = make2DArray(cols, rows);
+
 	for(let i = 0; i < cols; i++) {
 		for(let j = 0; j < rows; j++) {
 			let x = i * resolution;
@@ -36,5 +52,22 @@ function draw() {
 			}
 		}
 	}
-}
 
+	for(let i = 0; i < cols; i++) {
+		for(let j = 0; j < rows; j++) {
+			let state = grid[i][j];
+			// Count live neighbors!
+			let sum = 0;
+			let neighbors = countN(grid, i, j);
+
+			if (state == 0 && neighbors == 3) {
+				next[i][j] = 1;
+			} else if (state == 1 && (neighbors < 2 || neighbors > 3)) {
+				next[i][j] = 0;
+			} else {
+				next[i][j] = state;
+			}
+		}
+	}
+	grid = next;
+}
